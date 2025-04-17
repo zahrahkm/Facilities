1a487479bc466d69a5d16463bbce5c49e95fde63
zahra hakimi <dev.zahrahakimi777@gmail.com>
2025-04-17 18:22:26 +0200

    fix:a small padding issue reated to form container has been fixed.

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,17 @@ export default {
       inter: ['Inter', 'sans-serif'],
       nunito: ['Nunito', 'sans-serif'],
     },
+    container: {
+      center: true, 
+      padding: '1rem', 
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        xl: '1240px',
+        '2xl': '1240px',
+      },
+    }
   },
   plugins: [],
 }
